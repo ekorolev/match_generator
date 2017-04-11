751ca410f06280b82f6ddf284270c33082326e31
ekorolev <aksyline@gmail.com>
2017-04-11 11:56:55 +0300

    names and families base

---
+++ help/playerGenerator.js
@@ -0,0 +1,15 @@
+
+
+
+const Generator = options => {
+
+	let name 		= options.name;
+	let family		= options.family;
+	let power		= options.power;
+	let age			= options.age;
+	let position	= options.position;
+
+
+};
+
+module.exports = Generator;
